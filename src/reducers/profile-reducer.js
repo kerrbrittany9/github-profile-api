9acc5354f20fe16722bc457c8ab9983e4264eb13
Brittany Kerr <kerrbrittany9@gmail.com>
2017-09-29 10:12:31 -0700

    write reducers file with request and display repo actions

---
+++ src/reducers/profile-reducer.js
@@ -0,0 +1,33 @@
+import constants from "./../constants";
+
+const { defaultState, types } = constants;
+
+const reposById = (state = defaultState, action) => {
+  let profile;
+  let newProfile;
+  let newState;
+  switch (action.type) {
+    case types.REQUEST_REPO:
+      newDog = {
+        isFetching: true,
+        profileSearch: action.profileSearch,
+        profileId: action.profileId
+      };
+      newState = Object.assign({}, state, {[action.profileId]: newProfile
+      });
+      return newState;
+    case types.DISPLAY_REPO:
+      dog = state[action.profoleId];
+      newDog = Object.assign({}, profile, {
+        isFetching: false,
+        profileSearch: action.profileSearch,
+        profile: action.profile,
+        profileId: action.profileId
+      });
+      return newProfile;
+    default:
+      return state;
+  }
+}
+
+export default reposById;
